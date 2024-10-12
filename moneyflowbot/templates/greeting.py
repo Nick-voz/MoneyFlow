fa4855a5daee342b195f53468cf0bfc8e06d1766
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-12 19:57:13 +0300

    add greeting template

---
+++ moneyflowbot/templates/greeting.py
@@ -0,0 +1,8 @@
+def text_greeting():
+    text = ""
+    text += "Превет, я MoneyFlow..."
+    text += "Возможные команды:" + "\n"
+    text += "/start" + "\n"
+    text += "/sign_up" + "\n"
+    text += "/sign_in" + "\n"
+    return text
