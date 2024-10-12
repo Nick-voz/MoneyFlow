b943ecc30ca31ee61d61854991b047b9581418ea
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-12 19:33:51 +0300

    refactor user authentication
    fix import err
    throw away hidening chat_id
    add commit to session

--- moneyflowbot/utils/authentication.py
+++ moneyflowbot/utils/authentication.py
@@ -1,10 +1,9 @@
 from uuid import uuid4
 
 from models.base_model import engine
+from models.user import User
 from sqlalchemy import Select
 from sqlalchemy.orm import Session
-
-from moneyflowbot.models.user import User
 
 
 def genertate_global_user_id() -> str:
@@ -13,20 +12,20 @@ def genertate_global_user_id() -> str:
 
 def save_user(chat_id: int, global_user_id: str):
     user = User()
-    user.hiden_chat_id = hide_chat_id(chat_id)
+    user.chat_id = chat_id
     user.global_user_id = global_user_id
     with Session(engine) as session:
         session.add(user)
-
-
-def hide_chat_id(chat_id: int):
-    return hash(str(chat_id))
+        session.commit()
 
 
 def get_global_user_id(chat_id: int) -> str:
-    hiden_chad_id = hide_chat_id(chat_id)
-    selector = Select(User).where(User.hiden_chat_id == hiden_chad_id)
+    selector = Select(User).where(User.chat_id == chat_id)
+
     with Session(engine) as session:
         user: User = session.scalars(selector).one()
 
     return user.global_user_id
+
+
+def save_spend(): ...
