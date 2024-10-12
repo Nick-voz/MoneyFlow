c6e9f0859b5aa8a0ba884e0f490fe4853196662a
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-12 19:50:26 +0300

    add templates module
    it works on simple functions, but soon i think it will works on jinja2

--- moneyflowbot/handlers/command_handlers.py
+++ moneyflowbot/handlers/command_handlers.py
@@ -2,34 +2,48 @@ from telegram import Update
 from telegram.ext import Application
 from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
-
-from moneyflowbot.utils.authentication import genertate_global_user_id
+from utils.authentication import genertate_global_user_id
+from utils.authentication import get_global_user_id
+from utils.authentication import save_user
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    if chat_id is None:
-        return
     await context.bot.send_message(
-        chat_id=chat_id, text="Hellow I'm MoneFlow bot"
+        chat_id=chat_id, text="Hellow I'm MoneyFlow bot"
     )
 
 
 async def sign_up(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    if chat_id is None:
-        return
+    try:
+        global_user_id = get_global_user_id(chat_id)
+        await context.bot.send_message(
+            chat_id, text=f"your key is {global_user_id}"
+        )
+    except Exception as e:
+        global_user_id = None
+        print(e)
+        pass
 
-    global_user_id = genertate_global_user_id()
+    global_user_id = global_user_id or genertate_global_user_id()
 
     # TODO: it mastu be implement by templates
     text = "это твой ключ, который понадобится в бодущем для доступа к данным, это ключ мы не сохраняем, помни что при его утере твои данны будут навсегда утерены."
     text += "\n" + " - " + global_user_id
+
+    save_user(chat_id=chat_id, global_user_id=global_user_id)
     await context.bot.send_message(chat_id=chat_id, text=text)
 
 
 start_hendler = CommandHandler("start", start)
+sign_up_handler = CommandHandler("signUp", sign_up)
 
 
 def regester(app: Application) -> None:
-    app.add_handlers((start_hendler,))
+    app.add_handlers(
+        [
+            start_hendler,
+            sign_up_handler,
+        ]
+    )
---
+++ moneyflowbot/templates/sign_up.py
@@ -0,0 +1,8 @@
+def tetx_send_key(key: str) -> str:
+    res = ""
+    res += "Привет, вот твой ключ: "
+    res += key + "."
+    res += "\n"
+    res += "..."
+
+    return res
