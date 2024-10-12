10ed65736c306534e5c8a3ce26094ed1f9e9d2c8
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-12 19:58:15 +0300

    set up simple handlers with templates
    set up sign_up handler and start handler

--- moneyflowbot/handlers/command_handlers.py
+++ moneyflowbot/handlers/command_handlers.py
@@ -4,35 +4,22 @@ from telegram.ext import CommandHandler
 from telegram.ext import ContextTypes
 from utils.authentication import genertate_global_user_id
 from utils.authentication import get_global_user_id
-from utils.authentication import save_user
+
+from moneyflowbot.templates.greeting import text_greeting
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    await context.bot.send_message(
-        chat_id=chat_id, text="Hellow I'm MoneyFlow bot"
-    )
+    text = text_greeting()
+    await context.bot.send_message(chat_id=chat_id, text=text)
 
 
 async def sign_up(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    try:
-        global_user_id = get_global_user_id(chat_id)
-        await context.bot.send_message(
-            chat_id, text=f"your key is {global_user_id}"
-        )
-    except Exception as e:
-        global_user_id = None
-        print(e)
-        pass
 
-    global_user_id = global_user_id or genertate_global_user_id()
+    global_user_id = genertate_global_user_id()
+    text = text_send_key(global_user_id)
 
-    # TODO: it mastu be implement by templates
-    text = "это твой ключ, который понадобится в бодущем для доступа к данным, это ключ мы не сохраняем, помни что при его утере твои данны будут навсегда утерены."
-    text += "\n" + " - " + global_user_id
-
-    save_user(chat_id=chat_id, global_user_id=global_user_id)
     await context.bot.send_message(chat_id=chat_id, text=text)
 
 
