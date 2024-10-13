e7e867d403338c8500f3c53802cac30a70b4dba0
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-13 12:53:26 +0300

    change code forma, and add some todos for moving code in user model

--- moneyflowbot/utils/authentication.py
+++ moneyflowbot/utils/authentication.py
@@ -10,6 +10,7 @@ def genertate_global_user_id() -> str:
     return str(uuid4())
 
 
+# TODO: move this to user model
 def save_user(chat_id: int, key: str):
     user = User()
     user.chat_id = chat_id
@@ -19,15 +20,15 @@ def save_user(chat_id: int, key: str):
         session.commit()
 
 
+# TODO: move this to user model
 def get_global_user_id(chat_id: int) -> str:
     selector = Select(User).where(User.chat_id == chat_id)
-
     with Session(engine) as session:
         user: User = session.scalars(selector).one()
-
     return user.global_user_id
 
 
+# TODO: move this to user model
 def delete_user(chat_id: int):
     selector = Select(User).where(User.chat_id == chat_id)
     with Session(engine) as session:
