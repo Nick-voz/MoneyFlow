e054f1b863faa31eeb504170d9a0c1a9354fd70c
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-13 12:52:44 +0300

    change key interuct logic: now key hashing before saving in db

--- moneyflowbot/utils/authentication.py
+++ moneyflowbot/utils/authentication.py
@@ -10,10 +10,10 @@ def genertate_global_user_id() -> str:
     return str(uuid4())
 
 
-def save_user(chat_id: int, global_user_id: str):
+def save_user(chat_id: int, key: str):
     user = User()
     user.chat_id = chat_id
-    user.global_user_id = global_user_id
+    user.global_user_id = hash(key)
     with Session(engine) as session:
         session.add(user)
         session.commit()
