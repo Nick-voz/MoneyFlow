b5db393da90a73e0fcf0797c42b005e1f1971caf
«Nick-voz» <nik.vozisow@gmail.com>
2024-10-19 16:04:32 +0300

    move get_db_url() to utils, write doc string for this function

--- moneyflowbot/models/base_model.py
+++ moneyflowbot/models/base_model.py
@@ -1,18 +1,8 @@
-from os import getenv
-
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 
+from utils.utils import get_db_url
 
-def get_db_url() -> str:
-    DB_URL = getenv("DB_URL")
-    if DB_URL is None:
-        raise RuntimeError("can't load DB_URL")
-    return DB_URL
-
-
-load_dotenv()
 engine = create_engine(get_db_url())
 
 
--- moneyflowbot/utils/utils.py
+++ moneyflowbot/utils/utils.py
@@ -24,3 +24,23 @@ def get_token() -> str:
     if token is None:
         raise RuntimeError("can not load token from .env")
     return token
+
+
+def get_db_url() -> str:
+    """
+    Retrieve the database URL from environment variables.
+
+    This function attempts to get the database connection URL from
+    the environment variable named "DB_URL". If the environment
+    variable is not set, it raises a RuntimeError.
+
+    Returns:
+        str: The database connection URL.
+
+    Raises:
+        RuntimeError: If the "DB_URL" environment variable is not set.
+    """
+    db_url = getenv("DB_URL")
+    if db_url is None:
+        raise RuntimeError("can't load DB_URL")
+    return db_url
